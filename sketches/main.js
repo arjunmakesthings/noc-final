507720d5dc27b671d76a7ee76802963fa6a31988
arjun <82059571+arjunmakesthings@users.noreply.github.com>
2026-04-25 14:12:25 -0400

    get basics working.

--- sketches/main.js
+++ sketches/main.js
@@ -1,221 +1,141 @@
-//260414; noc-final-wip.
+/*
+new sketch now. 
 
-let word;
+the idea is for the system to fetch two words. 
 
-/* 
-we use the following states: 
-generate -> await -> evaluate -> result -> if right: go to generate; else if wrong: go to wait.
+one is given to the person to guess, and the other is being guessed live by the computer. 
+
+As of 2019, the average typing speed on a mobile phone was 36.2 wpm with 2.3% uncorrected errors—there were significant correlations with age, level of English proficiency, and number of fingers used to type.
+^ wiki: https://en.wikipedia.org/wiki/Words_per_minute
+
+the computer can guess once in 30s, and it takes 30s to arrive at a word. 
+
+the stages remain the same as wordle; only that i will have to account for two entities: human & computer. 
+
+stages: 
+welcome -> generate -> await -> evaluate -> all_correct / some_correct / all_wrong.
+
+each entitity will also have a local state.
 */
-let state = "null";
-let p_state = state;
 
-//text to speech stuff:
-let speech;
+//three entitites; so each have their own class.
+let human;
+let machine;
+let host;
 
-//dialogues are in key-value pairs.
-let dialogues = {
-  in_generate: [
-    "computer, guess this stupid word",
-    "hey you computer — guess the word i'm thinking of",
-  ],
-  in_await: [],
-  in_evaluate: [],
-  all_correct: ["ok ... now"],
-  some_correct: [
-    "nooooo, try again",
-    "ugh no you idiot",
-    "god no",
-    "close but not quite",
-  ],
-  all_wrong: [
-    "god you're so dumb",
-    "no you idiot",
-    "stupid shit work ... try again and do better",
-  ],
-};
-
-let input_str;
-
-let result = [];
-
-let attempts = [];
+let global_state = "null";
+let p_global_state = global_state; //for state change detection.
 
 function setup() {
-  // createCanvas(1000, 562); //in 16:9 aspect ratio.
-  createCanvas(windowWidth, windowHeight); //square to handle calculations better.
+  createCanvas(windowWidth, windowHeight);
 
-  speech = new p5.Speech();
-
-  //assign a random voice. we wait a little bit to run this block of code for it to get all the voices.
-  // setTimeout(() => {
-  //   let all_voices = speech.voices;
-  //   let voice = Math.floor(random(all_voices.length));
-  //   speech.setVoice(voice);
-  //   console.log("current voice:", all_voices[voice].name);
-  // }, 500);
+  host = new Host();
+  machine = new Machine();
+  human = new Human();
 }
 
 function draw() {
+  ui();
+
+  host.state_manager();
+  machine.state_manager();
+  human.state_manager();
+
+  if (global_state == "generate") {
+    background(255);
+  }
+}
+
+function ui() {
   background(0);
-
-  push();
-  textAlign (LEFT, TOP);  
-  textSize(16);
-  fill(255);
-  text("you're trying to guess a 5-letter word.", 50, 50);
-  fill(255, 200, 0);
-  text("yellow means right character; but wrong position.", 50, 50 + 28);
-  fill(0, 255, 0);
-  text("green means correct guess.", 50, 50 + 28 * 2);
-  pop(); 
-
-  for (let i = 0; i < attempts.length; i++) {
-    attempts[i].display();
-  }
-
-  if (state === "generate") {
-    state = "temp-hold"; //temp state to avoid looping multiple times (because it's in draw).
-
-    //clear previous attempts.
-    attempts = [];
-
-    fetch_word().then((result) => {
-      word = result;
-      console.log("garbled word -> " + word);
-    });
-
-    //say dialogue:
-    speech.speak(random(dialogues.in_generate));
-
-    //state change:
-    state = "await";
-
-    //prepare input string:
-    input_str = "";
-  } else if (state === "await") {
-    // state = "temp-hold"; //temp state to avoid looping multiple times (because it's in draw).
-
-    show_typing(input_str);
-
-    if (input_str.length === 5) {
-      //when it is 5, go to evaluate.
-      state = "evaluate";
-    }
-  } else if (state === "evaluate") {
-    result = [];
-
-    for (let i = 0; i < 5; i++) {
-      let c = input_str[i];
-
-      if (c === word[i]) {
-        result[i] = "correct";
-      } else if (word.includes(c)) {
-        result[i] = "wrong-pos";
-      } else {
-        result[i] = "wrong";
-      }
-    }
-
-    console.log(result);
-
-    attempts.push(new Attempt(input_str, result));
-
-    state = "result";
-  } else if (state === "result") {
-    let correct = result.filter((r) => r === "correct").length;
-    let wrong_pos = result.filter((r) => r === "wrong-pos").length;
-    let wrong_char = result.filter((r) => r === "wrong").length;
-
-    let dominant = Math.max(correct, wrong_pos, wrong_char);
-
-    if (dominant === correct) {
-      speech.speak(random(dialogues.all_correct));
-      state = "generate";
-    } else if (dominant === wrong_char) {
-      speech.speak(random(dialogues.all_wrong));
-      state = "await";
-    } else {
-      speech.speak(random(dialogues.some_correct));
-      state = "await";
-    }
-
-    //reset input.
-    input_str = "";
-  }
-
-  //state change:
-  if (state != p_state) {
-    console.log("state -> " + state);
-  }
-  p_state = state;
 }
 
-//show what is being typed.
-function show_typing(input_str) {
-  textSize(48);
-  textAlign(CENTER, CENTER);
-
-  for (let i = 0; i < input_str.length; i++) {
-    fill(255);
-    text(input_str[i], width / 2 - 100 + i * 50, height - 100);
-  }
-}
+function keyPressed() {}
 
 function mousePressed() {
-  //text to speech needs a user-action to begin everything. so, we keep this to start.
-  state = "generate";
+  //to initialize everything, since chrome needs user-input to start sound playback.
+  host.local_state = "welcome";
 }
 
-function keyPressed() {
-  if (state === "await") {
-    input_str += key;
-  }
-}
-
-async function fetch_word() {
-  let res = await fetch("https://random-word-api.herokuapp.com/word?length=5");
-  let data = await res.json();
-  word = data[0];
-
-  console.log("og word -> " + word);
-
-  //garble the word:
-  let chars = word.split("");
-  for (let i = chars.length - 1; i > 0; i--) {
-    let j = Math.floor(random(i + 1));
-    [chars[i], chars[j]] = [chars[j], chars[i]];
+class Human {
+  constructor() {
+    this.local_state = "null";
+    this.p_state = "null2";
   }
 
-  return chars.join("");
-
-  // //temp fallback:
-
-  // let temp_word = random(["pplea", "ppyah"]);
-
-  // return temp_word;
-}
-
-class Attempt {
-  constructor(word, result) {
-    this.word = word;
-    this.result = result;
-  }
-
-  display() {
-    let index = attempts.indexOf(this);
-    let y = 100 + index * 60;
-
-    textSize(48);
-    textAlign(CENTER, CENTER);
-
-    for (let i = 0; i < this.word.length; i++) {
-      let x = width / 2 - 100 + i * 50;
-
-      if (this.result[i] === "correct") fill(0, 255, 0);
-      else if (this.result[i] === "wrong-pos") fill(255, 200, 0);
-      else fill(120);
-
-      text(this.word[i], x, y);
+  state_manager() {
+    if (this.local_state != this.p_state) {
+      //a state change has happened.
+      if (this.local_state == "await_begin") {
+        this.await_begin();
+      }
     }
+    this.p_state = this.local_state;
+  }
+
+  await_begin() {
+    if (!this.readyBtn) {
+      this.readyBtn = createButton("ready");
+      this.readyBtn.position(width / 2 - 40, height / 2);
+
+      this.readyBtn.mousePressed(() => {
+        global_state = "generate";
+        this.readyBtn.remove();
+        this.readyBtn = null;
+      });
+    }
+  }
+}
+
+class Machine {
+  constructor() {
+    this.speech = new p5.Speech("Fred");
+    this.speech.setRate(0.9);
+
+    this.local_state = "null";
+    this.p_state = "null2";
+  }
+
+  state_manager() {
+    if (this.local_state != this.p_state) {
+      //a state change has happened.
+      if (this.local_state == "ready") {
+        this.speech.speak("i'm ready. i'm going to take you down.");
+      }
+    }
+    this.p_state = this.local_state;
+  }
+}
+
+class Host {
+  constructor() {
+    this.speech = new p5.Speech("Grandpa (English (United Kingdom))");
+    this.speech.setRate(0.9);
+    // this.speech.setVoice("Fred");
+
+    this.local_state = "null";
+    this.p_state = "null2";
+  }
+  state_manager() {
+    if (this.local_state != this.p_state) {
+      //a state change has happened.
+      if (this.local_state == "welcome") {
+        this.welcome();
+      }
+    }
+    this.p_state = this.local_state;
+  }
+
+  welcome() {
+    // this.speech.speak(
+    //   "welcome puny human ... we've been hearing your declarations on the news about humans being smarter than computers. let's put that to the test now; shall we? ......... you & the machine on your right have been assigned a random 5-letter word. the first one to guess wins ...... are you game?",
+    // );
+
+    this.speech.speak("tester");
+
+    setTimeout(() => {
+      machine.local_state = "ready";
+      human.local_state = "await_begin";
+    }, 2000); // delay in milliseconds
   }
 }
---
+++ sketches/wordle.js
@@ -0,0 +1,221 @@
+//260414; noc-final-wip.
+
+let word;
+
+/* 
+we use the following states: 
+generate -> await -> evaluate -> result -> if right: go to generate; else if wrong: go to wait.
+*/
+let state = "null";
+let p_state = state;
+
+//text to speech stuff:
+let speech;
+
+//dialogues are in key-value pairs.
+let dialogues = {
+  in_generate: [
+    "computer, guess this stupid word",
+    "hey you computer — guess the word i'm thinking of",
+  ],
+  in_await: [],
+  in_evaluate: [],
+  all_correct: ["ok ... now"],
+  some_correct: [
+    "nooooo, try again",
+    "ugh no you idiot",
+    "god no",
+    "close but not quite",
+  ],
+  all_wrong: [
+    "god you're so dumb",
+    "no you idiot",
+    "stupid shit work ... try again and do better",
+  ],
+};
+
+let input_str;
+
+let result = [];
+
+let attempts = [];
+
+function setup() {
+  // createCanvas(1000, 562); //in 16:9 aspect ratio.
+  createCanvas(windowWidth, windowHeight); //square to handle calculations better.
+
+  speech = new p5.Speech();
+
+  //assign a random voice. we wait a little bit to run this block of code for it to get all the voices.
+  // setTimeout(() => {
+  //   let all_voices = speech.voices;
+  //   let voice = Math.floor(random(all_voices.length));
+  //   speech.setVoice(voice);
+  //   console.log("current voice:", all_voices[voice].name);
+  // }, 500);
+}
+
+function draw() {
+  background(0);
+
+  push();
+  textAlign (LEFT, TOP);  
+  textSize(16);
+  fill(255);
+  text("you're trying to guess a 5-letter word.", 50, 50);
+  fill(255, 200, 0);
+  text("yellow means right character; but wrong position.", 50, 50 + 28);
+  fill(0, 255, 0);
+  text("green means correct guess.", 50, 50 + 28 * 2);
+  pop(); 
+
+  for (let i = 0; i < attempts.length; i++) {
+    attempts[i].display();
+  }
+
+  if (state === "generate") {
+    state = "temp-hold"; //temp state to avoid looping multiple times (because it's in draw).
+
+    //clear previous attempts.
+    attempts = [];
+
+    fetch_word().then((result) => {
+      word = result;
+      console.log("garbled word -> " + word);
+    });
+
+    //say dialogue:
+    speech.speak(random(dialogues.in_generate));
+
+    //state change:
+    state = "await";
+
+    //prepare input string:
+    input_str = "";
+  } else if (state === "await") {
+    // state = "temp-hold"; //temp state to avoid looping multiple times (because it's in draw).
+
+    show_typing(input_str);
+
+    if (input_str.length === 5) {
+      //when it is 5, go to evaluate.
+      state = "evaluate";
+    }
+  } else if (state === "evaluate") {
+    result = [];
+
+    for (let i = 0; i < 5; i++) {
+      let c = input_str[i];
+
+      if (c === word[i]) {
+        result[i] = "correct";
+      } else if (word.includes(c)) {
+        result[i] = "wrong-pos";
+      } else {
+        result[i] = "wrong";
+      }
+    }
+
+    console.log(result);
+
+    attempts.push(new Attempt(input_str, result));
+
+    state = "result";
+  } else if (state === "result") {
+    let correct = result.filter((r) => r === "correct").length;
+    let wrong_pos = result.filter((r) => r === "wrong-pos").length;
+    let wrong_char = result.filter((r) => r === "wrong").length;
+
+    let dominant = Math.max(correct, wrong_pos, wrong_char);
+
+    if (dominant === correct) {
+      speech.speak(random(dialogues.all_correct));
+      state = "generate";
+    } else if (dominant === wrong_char) {
+      speech.speak(random(dialogues.all_wrong));
+      state = "await";
+    } else {
+      speech.speak(random(dialogues.some_correct));
+      state = "await";
+    }
+
+    //reset input.
+    input_str = "";
+  }
+
+  //state change:
+  if (state != p_state) {
+    console.log("state -> " + state);
+  }
+  p_state = state;
+}
+
+//show what is being typed.
+function show_typing(input_str) {
+  textSize(48);
+  textAlign(CENTER, CENTER);
+
+  for (let i = 0; i < input_str.length; i++) {
+    fill(255);
+    text(input_str[i], width / 2 - 100 + i * 50, height - 100);
+  }
+}
+
+function mousePressed() {
+  //text to speech needs a user-action to begin everything. so, we keep this to start.
+  state = "generate";
+}
+
+function keyPressed() {
+  if (state === "await") {
+    input_str += key;
+  }
+}
+
+async function fetch_word() {
+  let res = await fetch("https://random-word-api.herokuapp.com/word?length=5");
+  let data = await res.json();
+  word = data[0];
+
+  console.log("og word -> " + word);
+
+  //garble the word:
+  let chars = word.split("");
+  for (let i = chars.length - 1; i > 0; i--) {
+    let j = Math.floor(random(i + 1));
+    [chars[i], chars[j]] = [chars[j], chars[i]];
+  }
+
+  return chars.join("");
+
+  // //temp fallback:
+
+  // let temp_word = random(["pplea", "ppyah"]);
+
+  // return temp_word;
+}
+
+class Attempt {
+  constructor(word, result) {
+    this.word = word;
+    this.result = result;
+  }
+
+  display() {
+    let index = attempts.indexOf(this);
+    let y = 100 + index * 60;
+
+    textSize(48);
+    textAlign(CENTER, CENTER);
+
+    for (let i = 0; i < this.word.length; i++) {
+      let x = width / 2 - 100 + i * 50;
+
+      if (this.result[i] === "correct") fill(0, 255, 0);
+      else if (this.result[i] === "wrong-pos") fill(255, 200, 0);
+      else fill(120);
+
+      text(this.word[i], x, y);
+    }
+  }
+}
